02ecf99d90da2eb00a6ee5c811757506e6df576a
EC2 Default User <ec2-user@ip-172-31-27-68.ap-northeast-1.compute.internal>
2022-09-05 10:54:39 +0000

    [応用課題2]テスト(リダイレクト先が、自分のユーザ詳細画面になっている)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(@user), notice: "You have updated user successfully."
     else
       render "edit"
     end
