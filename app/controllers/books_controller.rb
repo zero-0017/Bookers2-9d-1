a5444296e4bbd02d318ae571bc4d516cd675cab3
EC2 Default User <ec2-user@ip-172-31-27-68.ap-northeast-1.compute.internal>
2022-09-08 01:21:13 +0000

    [応用課題8d]投稿が新しい順、評価が高い順

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,6 +10,13 @@ class BooksController < ApplicationController
     @book = Book.new
     @books = Book.all
     @user = current_user
+   if params[:latest]
+     @books = Book.latest
+   elsif params[:star_count]
+     @books = Book.star_count
+   else
+     @books = Book.all
+   end
   end
 
   def create
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,9 @@ class Book < ApplicationRecord
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
   validates :star,presence:true
+
+  validates :star,presence:true
+
+  scope :latest, -> {order(created_at: :desc)}
+  scope :star_count, -> {order(star: :desc)}
 end
